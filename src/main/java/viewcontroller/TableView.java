fc9cf93b67d9e8c25bf3e8ebb5a5289439d65d94
Joana Becheva <joana.becheva@gmail.com>
2018-05-10 16:34:58 +0200

    Create TableView

---
+++ src/main/java/viewcontroller/TableView.java
@@ -0,0 +1,10 @@
+package main.java.viewcontroller;
+
+import javax.swing.*;
+
+public class TableView {
+    private JPanel tablePanel;
+    private JTable joinableLecturesTable;
+    private JScrollPane joinableLecturesPanel;
+}
+
