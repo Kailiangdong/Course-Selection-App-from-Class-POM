0af1fcc8cb800e0dee536ff902a89036fa348896
Stefan Vladov <stefan.vladov@tum.de>
2018-05-10 19:51:18 +0200

    Added the constructor for the SQLiteManager as well as a function to execute queries on it.

--- src/main/java/SQLiteManager/SQLiteManager.java
+++ src/main/java/SQLiteManager/SQLiteManager.java
@@ -1,59 +1,85 @@
 package main.java.SQLiteManager;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class SQLiteManager {
 
     private static final String JDBC_DRIVER = "org.sqlite.JDBC";
-    private static final String DB_URL = "jdbc:sqlite:test.db";
+    private static final String DB_URL = "jdbc:sqlite:cache.db";
+    private static final String DB_NAME = "cache.db";
 
+    /** Creates an SQLiteManager object
+     *  If the cache doesn't exist it creates a new one
+     *  TODO: If the cache is corrupted it wipes it
+     * @author Stefan Vladov
+     */
     public SQLiteManager() {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Failed loading JDBC_DRIVER " + e.toString());
+            System.exit(-1);
+        }
+        Path path = Paths.get(DB_NAME);
+        if (Files.notExists(path)) {
+            initializeCache();
+        }
+    }
 
+    /** Initializes the SQLite database with:
+     *      Lectures table - contains all lectures
+     *      Students table - contains all students
+     *      Attends table - contains all (student, lecture) pairs where a the student attends the lecture
+     */
+    private static void initializeCache() {
+        try(Connection c = DriverManager.getConnection(DB_URL); Statement stmt = c.createStatement()) {
+            String sql = "CREATE TABLE LECTURES " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " TITLE          TEXT    NOT NULL, " +
+                    " CHAIR          TEXT, " +
+                    " ECTS           INT, " +
+                    " SEMESTER       TEXT, " +
+                    " TIME           TEXT, " +
+                    " PLACE          TEXT, " +
+                    " LECTURER       TEXT, " +
+                    " GRADE_FACTOR   FLOAT " +
+                    ");";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE STUDENTS " +
+                    "(ID   INT PRIMARY KEY NOT NULL," +
+                    " NAME TEXT NOT NULL," +
+                    " MAJOR TEXT NOT NULL, " +
+                    " MINOR TEXT" +
+                    ");";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE ATTENDS " +
+                    "(STUDENT_ID INTEGER REFERENCES STUDENTS ON DELETE CASCADE," +
+                    " LECTURE_ID INTEGER REFERENCES LECTURES ON DELETE CASCADE," +
+                    " PRIMARY KEY (STUDENT_ID, LECTURE_ID));";
+            stmt.executeUpdate(sql);
+
+            // TODO A table which determines to which chair a major belongs to
+        } catch (SQLException e) {
+            System.out.println("Error executing query " + e.toString());
+            System.exit(-1);
+        }
+        System.out.println("Database created successfully");
+    }
+
+    public void executeQuery(String query) {
+        try(Connection c = DriverManager.getConnection(DB_URL); Statement stmt = c.createStatement()) {
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.println("Invalid query " + e.toString());
+        }
     }
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            //STEP 2: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL);
-
-            //STEP 4: Execute a query
-            System.out.println("Creating database...");
-            stmt = conn.createStatement();
-
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " AGE            INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
-            System.out.println("Database created successfully...");
-        }catch(SQLException se){
-            //Handle errors for JDBC
-            se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        }finally{
-            //finally block used to close resources
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se2){
-            }// nothing we can do
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
+        SQLiteManager manager = new SQLiteManager();
     }//end main
 }//end JDBCExample
