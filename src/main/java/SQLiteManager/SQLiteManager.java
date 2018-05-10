dcee55de5103c3caf6bf9cd4f2660b8473df3dcc
Stefan Vladov <stefan.vladov@tum.de>
2018-05-10 16:42:24 +0200

    Initial commit, added sqlite library and SQLiteManager class

---
+++ src/main/java/SQLiteManager/SQLiteManager.java
@@ -0,0 +1,59 @@
+package main.java.SQLiteManager;
+
+import java.sql.*;
+
+public class SQLiteManager {
+
+    private static final String JDBC_DRIVER = "org.sqlite.JDBC";
+    private static final String DB_URL = "jdbc:sqlite:test.db";
+
+    public SQLiteManager() {
+
+    }
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating database...");
+            stmt = conn.createStatement();
+
+            String sql = "CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            System.out.println("Database created successfully...");
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }//end main
+}//end JDBCExample
