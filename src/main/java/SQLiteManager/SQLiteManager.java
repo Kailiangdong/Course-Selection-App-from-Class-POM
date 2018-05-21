f81dc907dcc113de67703ae7f0a06b73a2abdfca
Stefan Vladov <stefan.vladov@tum.de>
2018-05-21 20:49:47 +0200

    Fixed the queryJoinableLectures to check both major and subject.

--- src/main/java/SQLiteManager/SQLiteManager.java
+++ src/main/java/SQLiteManager/SQLiteManager.java
@@ -212,7 +212,7 @@ public class SQLiteManager {
     public String[][] queryJoinableLectures(String name) throws SQLException{
         return this.executeQuery(new String[]{"l.ID", "l.TITLE"},
                 new String[]{"LECTURES l", "STUDENTS s", "ATTENDS at", "CHAIRS c"},
-                new String[]{"s.Name=" + "\"" + name + "\"", "s.major=c.subject", "c.chair=l.chair", "s.id=at.student_id", "at.lecture_id=l.id"});
+                new String[]{"s.Name=" + "\"" + name + "\"", "(s.major=c.subject or s.minor=c.subject)", "c.chair=l.chair", "s.id=at.student_id", "at.lecture_id=l.id"});
     }
 
     public static void main(String[] args) {
