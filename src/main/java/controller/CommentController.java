5a4c5714b0298e85f34e5f06acf6013181a09653
Kailiangdong <kailiang.dong@tum.de>
2018-06-07 09:41:54 +0200

    add query function and intergate it into comment array

--- src/main/java/controller/CommentController.java
+++ src/main/java/controller/CommentController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import SQLiteManager.QueryBuilder;
+import SQLiteManager.QueryType;
+import SQLiteManager.SQLiteManager;
+import org.sqlite.util.StringUtils;
 import view.CommentView;
 
 import javax.swing.*;
@@ -7,7 +11,10 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CommentController extends Controller {
@@ -20,6 +27,7 @@ public class CommentController extends Controller {
     CommentView view;
     LecturesTableController tableController;
     MenuController menuController;
+    private SQLiteManager sqLiteManager;
 
     String lectureId;
     String studentName;
@@ -31,20 +39,58 @@ public class CommentController extends Controller {
         this.view = new CommentView();
         this.tableController = tableController;
         this.menuController = menuController;
+        this.sqLiteManager = sqLiteManager;
         addListeners();
         addDummyComments();
         update();
     }
 
+    private QueryBuilder commentQuery(String lectureID) {
+        QueryBuilder query = new QueryBuilder(QueryType.SELECT);
+
+        query.addSelect("CONTENT", "COMMENTS");
+        query.addSelect("STUDENT_ID", "COMMENTS");
+        query.addSelect("TIME", "COMMENTS");
+        query.addSelect("DATE", "COMMENTS");
+
+
+        query.addFrom("COMMENTS");
+
+        query.addWhere("L.ID = " + lectureID);
+
+        return query;
+    }
+
+    public static ZonedDateTime changeStringToUTC(String DateTimeStr) {
+        DateTimeFormatter beijingFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("Asia/Shanghai"));
+        if (DateTimeStr == "") {
+            return null;
+        }
+        ZonedDateTime beijingDateTime = ZonedDateTime.parse(DateTimeStr, beijingFormatter);
+        return beijingDateTime.withZoneSameInstant(ZoneId.of("UTC"));
+    }
+
     private void addDummyComments() {
-        comments.add(new LectureComment("Stefan", "Lorem ipsum dolor sit amet, consectetur adipisici " +
+
+        String[][] queryResult = new String[0][];
+        try {
+            queryResult = sqLiteManager.executeQuery(commentQuery(lectureId));
+
+        } catch (SQLException e) {
+            System.out.println("Error executing show comment: " + e.toString());
+        }
+        for (int i = 0; i < queryResult.length; i++) {
+            comments.add(new LectureComment(queryResult[i][0], queryResult[i][1], changeStringToUTC(queryResult[i][2] + "," + queryResult[i][3]),
+                    false));
+        }
+/*        comments.add(new LectureComment("Stefan", "Lorem ipsum dolor sit amet, consectetur adipisici " +
                 "elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.", ZonedDateTime.now().minusHours(1), false));
         comments.add(new LectureComment("Markus", "Ut enim ad minim veniam, quis nostrud exercitation " +
                 "ullamco laboris nisi ut aliquid ex ea commodi consequat.", ZonedDateTime.now().minusHours(2), false));
         comments.add(new LectureComment("Robert", "Quis aute iure reprehenderit in voluptate velit esse " +
                 "cillum dolore eu fugiat nulla pariatur. ", ZonedDateTime.now().minusHours(1), false));
         comments.add(new LectureComment("Joana", "Excepteur sint obcaecat cupiditat non proident, sunt in " +
-                "culpa qui officia deserunt mollit anim id est laborum.", ZonedDateTime.now().minusHours(1), true));
+                "culpa qui officia deserunt mollit anim id est laborum.", ZonedDateTime.now().minusHours(1), true));*/
     }
 
     @Override
